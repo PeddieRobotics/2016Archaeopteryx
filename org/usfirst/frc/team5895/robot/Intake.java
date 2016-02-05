27b9a250727a1c7eb02ca2f1ac01713ab4d0e88a
CatherinePhillips <cphillips-19@peddie.org>
2016-02-05 16:53:34 -0500

    Made Intake.java more functional and way simpler

--- org/usfirst/frc/team5895/robot/Intake.java
+++ org/usfirst/frc/team5895/robot/Intake.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team5895.robot;
 
-import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 
@@ -8,32 +7,27 @@ public class Intake {
 	
 	Spark myMotor;
 	DigitalInput Sensor; 
-	PID myController;
-	Counter c;
-	private double kP;
-	private double kI;
-	private double kD;
-	private double dV;
 	
 	public Intake(){
 		myMotor = new Spark(0);
 		Sensor = new DigitalInput(0);
-		myController = new PID(kP, kI, kD, dV);
-		c = new Counter(8);
-		c.setDistancePerPulse(1);
-		c.setSamplesToAverage(2);
 		
 	}
 	
-	public void setSpeed(double speed){
-		myController.set(speed/60);
+	public void on(){
+		myMotor.set(1);
+	}
+	
+	public void shoot(){
+		if (Sensor.get()== true){
+			myMotor.set(1);
+		}
 	}
 	
 	public void update(){
 		if (Sensor.equals(true)){
-			myController.set(0);
+			myMotor.set(0);
 		}
-		myMotor.set(myController.getOutput(c.getRate()));
 	}
 	
 }
