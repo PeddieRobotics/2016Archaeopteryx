f8382b94d556e989a47ac969af00d17c32ddab2c
CatherinePhillips <cphillips-19@19-12-CPHILLIPS.lan.peddie.org>
2016-02-05 21:42:12 -0500

    Cleaned up Flywheel.java

--- org/usfirst/frc/team5895/robot/Flywheel.java
+++ org/usfirst/frc/team5895/robot/Flywheel.java
@@ -1,27 +1,21 @@
 package org.usfirst.frc.team5895.robot;
 
 import edu.wpi.first.wpilibj.Counter;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
 
 public class Flywheel {
 
 	Spark myMotor;
 	PID myController;
-	Encoder e;
 	Counter c;
 	private double Kp;
 	private double Ki;
 	private double Kd;
 	private double dV;
-	private double currentSpeed;
 	
 	public Flywheel(){
 		myMotor = new Spark(0);
 		myController = new PID(Kp, Ki, Kd, dV);
-		
-		e = new Encoder(0,1);
-		e.setDistancePerPulse(1);
 		
 		c = new Counter(8);
 		c.setDistancePerPulse(1);
@@ -34,11 +28,10 @@ public class Flywheel {
 	}
 	
 	public double getSpeed() {
-		return e.getRate()*60;
+		return c.getRate()*60;
 	}
 	
 	public void update() {
 		myMotor.set(myController.getOutput(c.getRate()));
-		currentSpeed= getSpeed();
 	}
 }
