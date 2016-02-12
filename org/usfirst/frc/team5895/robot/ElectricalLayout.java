69d15ccde8321c2bf1c472534ce6577a805b43bb
Antonio G Papa <antoniogpapa@gmail.com>
2016-02-12 16:08:56 -0500

    Formatting changes

--- org/usfirst/frc/team5895/robot/ElectricalLayout.java
+++ org/usfirst/frc/team5895/robot/ElectricalLayout.java
@@ -8,24 +8,18 @@ public class ElectricalLayout {
 	public static final int FLYWHEEL_MOTOR = 2;
 	public static final int INTAKE_MOTOR = 3;
 	public static final int TURRET_MOTOR = 4;
-<<<<<<< HEAD
-	public static final int ARM_SOLENOID = 0;
-	public static final int INTAKE_SOLENOID = 1;
-	public static final int DRIVE_DIGITALIO = 0;
-	public static final int DRIVE_DIGITALIO2 = 1;
-	public static final int FLYWHEEL_DIGITALIO = 2;
-	public static final int INTAKE_DIGITALIO = 3;
-	public static final int TURRET_DIGITALIO = 4;
-	public static final int TURRET_DIGITALIO2 = 5;
-=======
-	
-	
+
+
 	//Solenoids
 	public static final int ARM_SOLENOID = 0;
 	public static final int INTAKE_SOLENOID = 1;
 	
 	
 	//Digital Inputs
->>>>>>> origin/master
-	
+	public static final int DRIVE_DIGITALIO = 0;
+	public static final int DRIVE_DIGITALIO2 = 1;
+	public static final int FLYWHEEL_DIGITALIO = 2;
+	public static final int INTAKE_DIGITALIO = 3;
+	public static final int TURRET_DIGITALIO = 4;
+	public static final int TURRET_DIGITALIO2 = 5;	
 }
